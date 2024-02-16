d0b04fe55a5013fac25e54cac0e14cd106f3a01d
dhruvi6071 <dhruvi6071@gmail.com>
2024-02-16 22:23:14 +0530

    component-composition-as-solution-for-prop-drilling

--- cart-function/cart/src/components/Cart.jsx
+++ cart-function/cart/src/components/Cart.jsx
@@ -11,7 +11,7 @@ export default function Cart({ items, onUpdateItemQuantity }) {
       {items.length > 0 && (
         <ul id="cart-items">
           {items.map((item) => {
-            const formattedPrice = `$${item.price.toFixed(2)}`;
+            const formattedPrice = `$${item.price.toFixed(2)}`; 
 
             return (
               <li key={item.id}>
